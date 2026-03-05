5cba93324e04851dda6866437e9e06b16007cac3
Joel Pepper <pepper@bronze-deer.de>
2026-03-05 17:48:15 +0100

    fixup! feat: initial setup, deploy argocd, crossplane

--- hack/cluster-setup.sh
+++ hack/cluster-setup.sh
@@ -3,8 +3,11 @@ SCRIPT_DIR=`dirname $SCRIPT_PATH`
 
 
 kind create cluster --config $SCRIPT_DIR/cluster.yaml --wait=30s
+
+vcluster connect guest-crossplane || vcluster create guest-crossplane
+
 kubectl create ns argocd
 helm -n argocd template bootstrap-argocd $SCRIPT_DIR/../deps/argocd/vendor/chart/ -f $SCRIPT_DIR/../deps/argocd/values.yaml \
-| kubectl --context kind-vcluster-crossplane -n argocd apply --server-side -f -
+| kubectl -n argocd apply --server-side -f -
 jsonnet -y --tla-str "sourceTargetRevision=$(git rev-parse --abbrev-ref=strict HEAD)" manifests/bootstrap.jsonnet \
-| kubectl --context kind-vcluster-crossplane -n argocd apply --server-side -f -
+| kubectl -n argocd apply --server-side -f -
